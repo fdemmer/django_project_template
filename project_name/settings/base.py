a5fcd4f5fb47b1c153fdee5f88d0961b27e814d3
Florian Demmer <fdemmer@gmail.com>
2016-02-10 02:31:53 +0100

    Add project wide 'locale' dir to LOCALE_PATHS

--- project_name/settings/base.py
+++ project_name/settings/base.py
@@ -117,6 +117,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (
+    os.path.join(PROJECT_DIR, 'locale'),
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/{{ docs_version }}/howto/static-files/
